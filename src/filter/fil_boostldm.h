c3f006c2c4064df3426d82d330b993912f7a0fb2
Lily Mummert <lily.b.mummert@intel.com>
2006-10-20 04:29:58 +0000

    added definitions for filter operations

--- src/filter/fil_boostldm.h
+++ src/filter/fil_boostldm.h
@@ -10,5 +10,9 @@ typedef struct boostldm_config
   float   *features;  /* features computed on object */
 } boostldm_config_t;
 
+int f_init_boostldm(int numarg, char **args, int blob_len,
+                    void *blob, const char *fname, void **data);
+int f_eval_boostldm(lf_obj_handle_t ohandle, void *f_data);
+int f_fini_boostldm(void *data);
 
 #endif /*FIL_BOOSTLDM_H_*/
--- src/filter/fil_qaldm.h
+++ src/filter/fil_qaldm.h
@@ -10,4 +10,9 @@ typedef struct qaldm_config
   float   *features;  /* features computed on object */
 } qaldm_config_t;
 
+int f_init_qaldm(int numarg, char **args, int blob_len,
+                    void *blob, const char *fname, void **data);
+int f_eval_qaldm(lf_obj_handle_t ohandle, void *f_data);
+int f_fini_qaldm(void *data);
+
 #endif /*FIL_QALDM_H_*/
