23ecbf3affb53dfaac9d77a06e647baec583dba2
Lily Mummert <lily.b.mummert@intel.com>
2006-10-20 04:26:08 +0000

    read case data

--- src/app/roimap.c
+++ src/app/roimap.c
@@ -20,11 +20,13 @@
 #include <dirent.h>
 #include <sys/param.h>
 
-#include <gtk/gtk.h>
-
 #include "roimap.h"
 
 #define MAPFILENAME "Truthfile.txt"
+#define MAXLINELEN 256
+#define MAXATTRLEN 32
+#define SEPARATORS " \t"
+#define ATTR_SEPS "\"=\n"
 
 /*
  *  get a region of interest record
@@ -37,6 +39,14 @@ roi_t *get_roi_record(char *mapFileName, char *studyID) {
   char *image_name;
   int center_x;
   int center_y;
+  int birad;
+  int density;
+  int subtlety;
+  int age;
+  int biopsy;
+  int shape;
+  int margin;
+  
   roi_t *roi = NULL;
 
   // look for file names with the study ID
@@ -48,8 +58,8 @@ roi_t *get_roi_record(char *mapFileName, char *studyID) {
     if (next_token != NULL) {
       // is this a case number?
       if (isdigit(next_token[0])) {
-	// skip past the case number 
-	next_token = strtok(NULL, SEPARATORS);
+		// skip past the case number 
+		next_token = strtok(NULL, SEPARATORS);
       }
     }
     if (next_token != NULL) {
@@ -61,7 +71,7 @@ roi_t *get_roi_record(char *mapFileName, char *studyID) {
     if (next_token != NULL) {
       g_debug("center_x %s", next_token);
       if (isdigit(next_token[0])) {
-	center_x = atoi(next_token);
+		center_x = atoi(next_token);
       }
       // center y
       next_token = strtok(NULL, SEPARATORS);
@@ -69,12 +79,77 @@ roi_t *get_roi_record(char *mapFileName, char *studyID) {
     if (next_token != NULL) {
       g_debug("center_y %s", next_token);
       if (isdigit(next_token[0])) {
-	center_y = atoi(next_token);
-	record_complete = TRUE;
-      }
+		center_y = atoi(next_token);
+ 	    next_token = strtok(NULL, SEPARATORS);
+     }
     }
-    
-    if (!record_complete)
+    if (next_token != NULL) {
+    	g_debug("birad %s", next_token);
+ 	    if (isdigit(next_token[0])) {
+ 	    	birad = atoi(next_token);
+ 		    next_token = strtok(NULL, SEPARATORS);
+     	}
+    }
+    if (next_token != NULL) {
+    	g_debug("subtlety %s", next_token);
+ 	    if (isdigit(next_token[0])) {
+ 	    	subtlety = atoi(next_token);
+ 		    next_token = strtok(NULL, SEPARATORS);
+     	}
+    }
+    if (next_token != NULL) {
+    	g_debug("density %s", next_token);
+ 	    if (isdigit(next_token[0])) {
+ 	    	density = atoi(next_token);
+ 		    next_token = strtok(NULL, SEPARATORS);
+     	}
+    }
+    if (next_token != NULL) {
+    	g_debug("age %s", next_token);
+ 	    if (isdigit(next_token[0])) {
+ 	    	age = atoi(next_token);
+ 		    next_token = strtok(NULL, SEPARATORS);
+     	}
+    }
+    if (next_token != NULL) {
+    	g_debug("biopsy %s", next_token);
+ 	    if (strcmp(next_token, "MALIGNANT") == 0) {
+ 	    	biopsy = MALIGNANT;
+ 	    } else {
+ 	    	biopsy = BENIGN;
+ 	    }
+ 		next_token = strtok(NULL, SEPARATORS);
+    }
+    if (next_token != NULL) {
+    	g_debug("shape %s", next_token);    	
+ 	    if (strcmp(next_token, "ROUND") == 0) {
+ 	    	shape = ROUND;
+ 	    } else if (strcmp(next_token, "OVAL") == 0) {
+ 	    	shape = OVAL;
+ 	    } else if (strcmp(next_token, "LOBULATED") == 0) {
+ 	    	shape = LOBULATED;
+ 	    } else {
+ 	    	shape = IRREGULAR;
+ 	    }
+ 		next_token = strtok(NULL, SEPARATORS);
+    }
+    if (next_token != NULL) {
+    	g_debug("margin %s", next_token);    	
+ 	    if (strcmp(next_token, "SPICULATED") == 0) {
+ 	    	margin = SPICULATED;
+ 	    } else if (strcmp(next_token, "ILLDEINFED") == 0) {
+ 	    	margin = ILLDEFINED;
+ 	    } else if (strcmp(next_token, "MICROLOBULATED") == 0) {
+ 	    	margin = MICROLOBULATED;
+ 	    } else if (strcmp(next_token, "CIRCUMSCRIBED") == 0){
+ 	    	margin = CIRCUMSCRIBED;
+ 	    } else {
+ 	    	margin = OBSCURED;
+ 	    }
+ 		record_complete = TRUE;
+    }
+
+   if (!record_complete)
       continue;
 
     // check study id for match
@@ -86,6 +161,13 @@ roi_t *get_roi_record(char *mapFileName, char *studyID) {
       roi->roi_image_name = NULL;
       roi->center_x = center_x;
       roi->center_y = center_y;
+      roi->subtlety = subtlety;
+      roi->birad = birad;
+      roi->density = density;
+      roi->age = age;
+      roi->biopsy = biopsy;
+      roi->shape = shape;
+      roi->margin = margin;
       roi->pixbuf = NULL;
       roi->attrs = NULL;
       break;
@@ -97,6 +179,31 @@ roi_t *get_roi_record(char *mapFileName, char *studyID) {
   return(roi);
 }
 
+void get_roi_attrs(char *attrFileName, GHashTable *ht) {
+
+  char line[MAXLINELEN];
+  char *attr = NULL;
+  char *value = NULL;
+  char *k, *v;
+
+  FILE *fp = fopen(attrFileName, "r");
+  while (fgets(line, MAXLINELEN, fp) != NULL) {
+    attr = strtok(line, ATTR_SEPS);
+    value = strtok(NULL, ATTR_SEPS);
+    if (attr != NULL && value != NULL) {
+    	k = malloc(MAXATTRLEN);
+    	v = malloc(MAXATTRLEN);
+    	strncpy(k, attr, MAXATTRLEN);
+    	strncpy(v, value, MAXATTRLEN);
+    	g_debug("attribute %s = %s", k, v);
+    	g_hash_table_insert(ht, k, v);
+    }
+  }
+
+  fclose(fp);
+  return;
+}
+
 
 roi_t *get_roi(char *path) {
 
@@ -104,6 +211,7 @@ roi_t *get_roi(char *path) {
   char imageName[MAXPATHLEN];
   char studyID[MAXNAMLEN];
   char mapFileName[MAXPATHLEN];
+  char attrFileName[MAXPATHLEN];
   roi_t *roi = NULL;
   GError *err = NULL;
   
@@ -134,7 +242,7 @@ roi_t *get_roi(char *path) {
     struct dirent *de;
     while ((de = readdir(dp)) != NULL) {
       if (strstr(de->d_name, studyID) &&
-	  strncmp(de->d_name, studyID, strlen(studyID))) {
+	 	  strncmp(de->d_name, studyID, strlen(studyID))) {
 		strcat(imageName, de->d_name);
 
 		roi->roi_image_name = (char *) malloc(strlen(imageName)+1);
@@ -150,7 +258,18 @@ roi_t *get_roi(char *path) {
 	    }	
 	    
 	    // load features from attr file
-	    roi->attrs = g_hash_table_new();
+	    strcpy(attrFileName, roi->roi_image_name);
+   		strcat(attrFileName, ".text_attr");	    
+	    g_debug("loading ROI attributes from %s", attrFileName);
+	    roi->attrs = g_hash_table_new(g_str_hash, g_str_equal);
+	    get_roi_attrs(attrFileName, roi->attrs);
+	    if (g_hash_table_size(roi->attrs) == 0) {
+	    	g_critical("no attributes found for %s", roi->roi_image_name);
+	    }
+	    
+	    g_debug("loaded %d ROI attributes from %s", 
+	    		g_hash_table_size(roi->attrs),
+	    		attrFileName);
 		break;
       }
     }
@@ -160,10 +279,20 @@ roi_t *get_roi(char *path) {
   return(roi);
 }
 
+
+gboolean remove_roi_attrs(gpointer key, gpointer value, gpointer user_data) {
+	free((char *) key);
+	free((char *) value);
+	return TRUE;
+}
+
+
 void free_roi(roi_t *r) {
-    g_object_unref(r->pixbuf);
     free(r->roi_image_name);
     free(r->full_image_name);
+    g_object_unref(r->pixbuf);
+    g_hash_table_foreach_remove(r->attrs, remove_roi_attrs, NULL);
+    g_hash_table_destroy(r->attrs);
     free(r);
 }
 
