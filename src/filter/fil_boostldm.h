4b17a90a92f1be96a84011e6f769f78dc6e580f4
Lily Mummert <lily.b.mummert@intel.com>
2006-12-27 15:45:37 +0000

    updated alpha values

--- src/filter/fil_boostldm.h
+++ src/filter/fil_boostldm.h
@@ -1,3 +1,17 @@
+/*
+ * MassFind: A Diamond application for exploration of breast tumors
+ *
+ * Copyright (c) 2006 Intel Corporation. All rights reserved.
+ * Additional copyrights may be listed below.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution in the file named LICENSE.
+ *
+ * Technical and financial contributors are listed in the file named
+ * CREDITS.
+ */
+ 
 #ifndef FIL_BOOSTLDM_H_
 #define FIL_BOOSTLDM_H_
 
