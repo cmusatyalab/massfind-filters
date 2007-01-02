bb5400dac75cdeb5b694b53dffab6e67c2f3f660
Lily Mummert <lily.b.mummert@intel.com>
2007-01-02 22:09:58 +0000

    common include for mass region of interest features

---
+++ src/common/roi_features.h
@@ -0,0 +1,18 @@
+#ifndef ROI_FEATURES_H_
+#define ROI_FEATURES_H_
+
+// constants for ROI text attributes
+
+#define MAXFEATURELEN 80
+#define MAXFNAMELEN 10
+
+#define UPMC_PREFIX "upmc"	// visual features
+#define EDMF_PREFIX "upmc"	// normalized features for euclidian distance
+#define BDMF_PREFIX "bdmf"	// additional features for boosted LDM distance
+
+#define NUM_UPMC "numupmc"
+#define NUM_EDMF "numupmc"
+#define NUM_BDMF "numbdmf"
+
+
+#endif /*ROI_FEATURES_H_*/
