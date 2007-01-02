0efba8b53d8d760041950744b4fe70814f9cb368
Lily Mummert <lily.b.mummert@intel.com>
2007-01-02 22:18:37 +0000

    filter for visual features

---
+++ src/filter/fil_visual.h
@@ -0,0 +1,33 @@
+/*
+ * MassFind: A Diamond application for exploration of breast tumors
+ *
+ * Copyright (c) 2006 Intel Corporation. All rights reserved.
+ * Additional copyrights may be listed below.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution in the file named LICENSE.
+ *
+ * Technical and financial contributors are listed in the file named
+ * CREDITS.
+ */
+
+
+#ifndef FIL_VISUAL_H_
+#define FIL_VISUAL_H_
+
+
+typedef struct visual_config
+{
+  int  numFeatures;
+  float   *features;  /* features computed on object */
+  float size_threshold;
+  float circularity_threshold;
+} visual_config_t;
+
+int f_init_visual(int numarg, char **args, int blob_len,
+                    void *blob, const char *fname, void **data);
+int f_eval_visual(lf_obj_handle_t ohandle, void *f_data);
+int f_fini_visual(void *data);
+
+#endif /*FIL_VISUAL_H_*/
