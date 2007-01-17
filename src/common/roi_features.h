125e34ac14bfbc54ce0231a5d34f63297be597e2
Lily Mummert <lily.b.mummert@intel.com>
2007-01-17 21:23:45 +0000

    updates for new features

--- src/common/roi_features.h
+++ src/common/roi_features.h
@@ -6,13 +6,15 @@
 #define MAXFEATURELEN 80
 #define MAXFNAMELEN 10
 
-#define UPMC_PREFIX "upmc"	// visual features
-#define EDMF_PREFIX "upmc"	// normalized features for euclidian distance
-#define BDMF_PREFIX "bdmf"	// additional features for boosted LDM distance
+#define UPMC_PREFIX "rawf"	// visual features
+#define EDMF_PREFIX "edmf"	// normalized features for euclidian distance
+#define BDMF_PREFIX "bdmf"	// features for boosted LDM distance
+#define CONTOUR_X_PREFIX "contourx" // mass contour features
+#define CONTOUR_Y_PREFIX "contoury" // a pair of these is a point
 
-#define NUM_UPMC "numupmc"
-#define NUM_EDMF "numupmc"
+#define NUM_UPMC "numrawf"
+#define NUM_EDMF "numedmf"
 #define NUM_BDMF "numbdmf"
-
+#define NUM_CONTOUR "numcontour"  // number of POINTS
 
 #endif /*ROI_FEATURES_H_*/
