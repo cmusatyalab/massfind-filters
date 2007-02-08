5124c450f14d93ca908f7e5bd4d69b966d5ed140
Lily Mummert <lily.b.mummert@intel.com>
2007-02-08 14:47:46 +0000

    changes for mass contours and mutual information

--- src/common/roi_features.h
+++ src/common/roi_features.h
@@ -3,18 +3,19 @@
 
 // constants for ROI text attributes
 
-#define MAXFEATURELEN 80
-#define MAXFNAMELEN 20
-
 #define UPMC_PREFIX "rawf"	// visual features
 #define EDMF_PREFIX "edmf"	// normalized features for euclidian distance
 #define BDMF_PREFIX "bdmf"	// features for boosted LDM distance
-#define CONTOUR_X_PREFIX "contourx" // mass contour features
-#define CONTOUR_Y_PREFIX "contoury" // a pair of these is a point
+#define CONTOUR_X 	"contourx" // mass contour features
+#define CONTOUR_Y	"contoury" // together these are a list of points
+#define MI_MEAN		"imean"	 // mean pixel intensity
+#define MI_STDEV	"istdev" // stdev of pixel intensity
 
 #define NUM_UPMC "numrawf"
 #define NUM_EDMF "numedmf"
 #define NUM_BDMF "numbdmf"
 #define NUM_CONTOUR "numcontour"  // number of POINTS
+
+#define CONTOUR_DELIM ';'	// delimiter in contour point string
 
 #endif /*ROI_FEATURES_H_*/
